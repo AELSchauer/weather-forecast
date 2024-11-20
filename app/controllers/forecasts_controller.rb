be5cb17f6be446c73118d19c75d3eb8fe56d5f35
Ashley Schauer <aelschauer@gmail.com>
2024-11-20 14:45:50 -0700

    add FE for current forecast

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,5 +1,17 @@
 class ForecastsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @greeting = "Hello, World!"
+  end
+
+  def create
+    weather_data = Api::OpenWeatherService.weather(lat: params[:location][:lat], lon: params[:location][:lon])
+
+    render json: weather_data.to_json
+    # respond_to do |format|
+    #   format.html
+    #   format.json { render json: @user }
+    # end
   end
 end
